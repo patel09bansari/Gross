6fcfd43d310d94a7ec74cf355ae19387a6470185
tusharmali058 <tusharmali058@gmail.com>
2025-12-16 22:20:56 -0800

    Initial Commit

---
+++ Gross/GrossApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrossApp.swift
+//  Gross
+//
+//  Created by Student on 16/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct GrossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
